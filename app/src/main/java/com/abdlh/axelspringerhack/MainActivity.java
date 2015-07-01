af8c7d97fcde91875d58daef15a7fa0386e3aa94
Kohlwage, Christian <christian.kohlwage@bild.de>
2015-07-01 11:54:02 +0200

    first draft ui

--- app/src/main/java/com/abdlh/axelspringerhack/MainActivity.java
+++ app/src/main/java/com/abdlh/axelspringerhack/MainActivity.java
@@ -1,17 +1,44 @@
 package com.abdlh.axelspringerhack;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.abdlh.axelspringerhack.UI.Fragments.PointsOfInterestFragment;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.baselayout);
+        initActionBar();
+        showPoiListFragment();
+    }
+
+    protected void initActionBar()
+    {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null)
+        {
+            setSupportActionBar(toolbar);
+
+        }
+
+        final ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
     }
 
     @Override
@@ -34,5 +61,21 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showPoiListFragment()
+    {
+        final FragmentManager mg = getSupportFragmentManager();
+        Fragment fragment = mg.findFragmentByTag(PointsOfInterestFragment.class.getSimpleName());
+        if (fragment == null)
+        {
+            final PointsOfInterestFragment poiListFragment = new PointsOfInterestFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content_fragment, poiListFragment, PointsOfInterestFragment.class.getSimpleName());
+            transaction.addToBackStack(PointsOfInterestFragment.class.getSimpleName());
+            transaction.commit();
+//            Log.d(TAG, "MeineBildFragment added to backstack");
+        }
+
     }
 }
--- app/src/main/java/com/abdlh/axelspringerhack/Presenters/PointsOfInterestPresenterImpl.java
+++ app/src/main/java/com/abdlh/axelspringerhack/Presenters/PointsOfInterestPresenterImpl.java
@@ -1,7 +1,31 @@
 package com.abdlh.axelspringerhack.Presenters;
 
+import android.content.Context;
+
+import com.abdlh.axelspringerhack.Model.Element;
+import com.abdlh.axelspringerhack.UI.Listners.onLoadingListner;
+
+import java.util.List;
+
 /**
  * Created by Abdellah on 7/1/15.
  */
 public class PointsOfInterestPresenterImpl implements PointsOfInterestPresenter{
+    @Override
+    public List<Element<?>> getMedications(onLoadingListner onLoadingListner)
+    {
+        return null;
+    }
+
+    @Override
+    public void onStart(Context context)
+    {
+
+    }
+
+    @Override
+    public void onItemClick(Element<?> element, Context context)
+    {
+
+    }
 }
